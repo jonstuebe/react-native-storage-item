0ef5a205cd46c6dd7c310f1cf79a7ed40f0b37a8
Jon Stuebe <jstuebe@gmail.com>
2019-11-16 18:12:02 -0700

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from 'react';
+
+export interface StorageItemOpts {
+  key: string;
+  initialValue?: any;
+  provider: any;
+  transform?: {
+    get: (value: any) => any;
+    set: (value: any) => any;
+  };
+  type?: any;
+}
+
+export default class StorageItem {
+  key: string;
+  value: any;
+  provider: any;
+  _inferredType: any;
+  getTransform: (value: any) => any;
+  setTransform: (value: any) => any;
+
+  constructor(opts: StorageItemOpts) {
+    this.key = opts.key;
+    this.provider = opts.provider;
+    this._inferredType = opts.type
+      ? opts.type
+      : opts.initialValue
+      ? typeof opts.initialValue
+      : null;
+    this.getTransform =
+      (opts.transform && opts.transform.get) || this._defaultGetTransform;
+    this.setTransform =
+      (opts.transform && opts.transform.set) || this._defaultSetTransform;
+    this.value = this.getTransform(
+      opts.initialValue === false
+        ? opts.initialValue
+        : opts.initialValue || null
+    );
+  }
+  _defaultGetTransform(value: any) {
+    switch (this._inferredType) {
+      case 'string':
+      case 'number':
+        return value;
+      default:
+        return JSON.parse(value);
+    }
+  }
+  _defaultSetTransform(value: any) {
+    switch (this._inferredType) {
+      case 'string':
+      case 'number':
+        return value;
+      default:
+        return JSON.stringify(value);
+    }
+  }
+  async init() {
+    const value = await this.provider.getItem(this.key);
+    if (value !== null) {
+      this.value = this.getTransform(value);
+    }
+    return;
+  }
+  set(newValue: any) {
+    this.value = this.setTransform(newValue);
+    this.provider.setItem(this.key, this.value);
+    return this.value;
+  }
+  remove() {
+    this.value = undefined;
+    this.provider.removeItem(this.key);
+  }
+}
+
+export const useStorageState = (storageItem: StorageItem) => {
+  const [state, setState] = useState(storageItem.value);
+
+  useEffect(() => {
+    storageItem.set(state);
+  }, [state]);
+
+  return [state, setState];
+};
+
+export const createStorageState = (storageItem: StorageItem) => {
+  return () => useStorageState(storageItem);
+};
---
+++ test/storageItem.test.tsx
@@ -0,0 +1,39 @@
+import { AsyncStorage } from 'react-native';
+import StorageItem from '../src';
+
+describe('StorageItem', () => {
+  it('value gets set based off of initial value', () => {
+    const fooStorage = new StorageItem({
+      key: 'foo',
+      initialValue: 'bar',
+      type: 'string',
+      provider: AsyncStorage,
+    });
+
+    expect(fooStorage.value).toBe('bar');
+  });
+  it('setting a new value to storage works', async () => {
+    const fooStorage = new StorageItem({
+      key: 'foo',
+      initialValue: 'bar',
+      type: 'string',
+      provider: AsyncStorage,
+    });
+
+    fooStorage.set('a different value');
+
+    expect(AsyncStorage.getItem('foo')).resolves.toEqual('a different value');
+  });
+  it('deleting value is successful', () => {
+    const fooStorage = new StorageItem({
+      key: 'foo',
+      initialValue: 'bar',
+      type: 'string',
+      provider: AsyncStorage,
+    });
+
+    fooStorage.remove();
+
+    expect(AsyncStorage.getItem('foo')).resolves.toEqual(null);
+  });
+});
